2317bb20f16410bc2352acce16a9a903064f4539
Sani Ranpariya <saniranpariya456@gmail.com>
2022-02-25 12:18:10 +0530

    Add patient done

---
+++ src/main/webapp/healthcareapp/src/app/components/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+
+  search:any;
+
+  options = [
+    { value: '1', label: 'Option 1' },
+    { value: '2', label: 'Option 2' },
+    { value: '3', label: 'Option 3' },
+  ];
+
+  constructor() { } 
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+
+  }
+}
---
+++ src/main/webapp/healthcareapp/src/app/services/patient.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class PatientService {
+
+  url = 'http://localhost:5050';
+
+  constructor(private httpCLient:HttpClient) { }
+
+  getAllPatients(){
+    let token = localStorage.getItem("token");
+    console.log(token);
+    let header = new HttpHeaders(
+      {
+        Authorization  : "Bearer " + token
+      }
+    )
+    return this.httpCLient.get(`${this.url}/getAllPatients`,{'headers':header});
+  }
+
+  addPatient(patientDetails:any){
+    let token = localStorage.getItem("token");
+    console.log(token);
+    let header = new HttpHeaders(
+      {
+        Authorization  : "Bearer " + token
+      }
+    )
+    return this.httpCLient.post(`${this.url}/addPatient`,patientDetails,{'headers':header});
+  }
+}
