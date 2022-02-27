91dcf790c394b8ed330ffddf7a522873c4bddfcd
Sani Ranpariya <saniranpariya456@gmail.com>
2022-02-27 18:34:41 +0530

    Search functionality and edit patient details and UI update

---
+++ src/main/webapp/healthcareapp/src/app/components/consultation-details/consultation-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-details',
+  templateUrl: './consultation-details.component.html',
+  styleUrls: ['./consultation-details.component.css']
+})
+export class ConsultationDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
