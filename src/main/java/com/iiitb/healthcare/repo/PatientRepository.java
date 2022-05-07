2c0ab08f13a61cbb34b3f33f60e641c2aa441f7f
Monish <monishkumarm@outlook.com>
2022-05-07 22:24:19 +0530

    REFACTORING: Intellij suggestions

--- src/main/java/com/iiitb/healthcare/repo/PatientRepository.java
+++ src/main/java/com/iiitb/healthcare/repo/PatientRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
-public interface PatientRepository extends JpaRepository<PatientEntity,Integer> {
+public interface PatientRepository extends JpaRepository<PatientEntity, Integer> {
 
-    @Query(value="select * from (SELECT * FROM Patient P INNER JOIN UserPermissionPatient UPP ON P.Id = UPP.PatientId WHERE UPP.UserId = ?1 AND UPP.CanView=1) `PU*`",nativeQuery = true)
+    @Query(value = "SELECT * FROM Patient P INNER JOIN UserPermissionPatient UPP ON P.Id = UPP.PatientId WHERE UPP.UserId = ?1 AND UPP.CanView=1", nativeQuery = true)
     List<PatientEntity> findAllPatientByUser(long id);
 
     List<PatientEntity> findById(long id);
